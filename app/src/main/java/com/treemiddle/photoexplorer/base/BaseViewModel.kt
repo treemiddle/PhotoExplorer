d3dd748e476dfe993fb470d01d43dfd59dc5416b
treemiddle <wj122791@gmail.com>
2026-07-16 22:59:57 +0900

    ViewEvent + ViewState + ViewSideEffect + BaseViewModel을 추가합니다.

---
+++ app/src/main/java/com/treemiddle/photoexplorer/base/BaseViewModel.kt
@@ -0,0 +1,40 @@
+package com.treemiddle.photoexplorer.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModelV4<Event : ViewEvent, UiState : ViewState, Effect : ViewSideEffect> :
+    ViewModel() {
+    val viewState: StateFlow<UiState> get() = _viewState
+    val effect get() = _effect.receiveAsFlow()
+
+    private val initialState: UiState by lazy {
+        setInitialState()
+    }
+
+    private val _viewState: MutableStateFlow<UiState> by lazy {
+        MutableStateFlow(value = initialState)
+    }
+
+    private val _effect: Channel<Effect> = Channel()
+
+    abstract fun setInitialState(): UiState
+    abstract fun handleEvents(event: Event)
+
+    protected fun setState(reducer: UiState.() -> UiState) {
+        _viewState.update {
+            viewState.value.reducer()
+        }
+    }
+
+    protected fun setEffect(builder: () -> Effect) {
+        val effectValue = builder()
+        viewModelScope.launch { _effect.send(element = effectValue) }
+    }
+}
---
+++ app/src/main/java/com/treemiddle/photoexplorer/base/ViewEvent.kt
@@ -0,0 +1,3 @@
+package com.treemiddle.photoexplorer.base
+
+interface ViewEvent
